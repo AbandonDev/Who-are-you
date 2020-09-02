f20c24c1a0048f20395eef5ff4d99669d1343d49
AbandonDev <66959398+AbandonDev@users.noreply.github.com>
2020-09-02 22:48:50 +0600

    Update page.cpp

--- src/page.cpp
+++ src/page.cpp
@@ -2,7 +2,15 @@
 
 void page::display()
 {
+  #if defined(__MINGW32__)
+  system("cls");
+  #elif defined(__MINGW64__)
+  system("cls");
+  #elif defined(_WIN32)
+  system("cls");
+  #else
   system("clear");
+  #endif
 
   std::cout << pattern + "\n\n";
   std::cout << header  + "\n\n";
